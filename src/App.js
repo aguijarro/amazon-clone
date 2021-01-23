813aab7bc24c0fa7d888f6b681c50e85506d1066
aguijarro <ec.andres.guijarro@gmail.com>
2021-01-23 05:53:44 -0500

    Add: CartItems components, CartTotal components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import CartItem from "./components/CartItems";
+import CartTotal from "./components/CartTotal";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header title="Amazon Cart" />
+      <div className="App-main">
+        <CartItem />
+        <CartTotal />
+      </div>
     </div>
   );
 }
---
+++ src/components/CartItems.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CartItems.css";
+
+const CartItem = () => {
+  return (
+    <div className="CartItems">
+      <h1>Car Items</h1>
+    </div>
+  );
+};
+
+export default CartItem;

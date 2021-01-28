aa7b4a442d42b77a426bfba404211c490f204619
aguijarro <ec.andres.guijarro@gmail.com>
2021-01-28 00:33:19 -0500

    ADD: CartTotal HTML/Style

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./CartTotal.css";
 
 const CartTotal = () => {
-  return <div className="CartTotal"></div>;
+  return (
+    <div className="CartTotal">
+      <h3>
+        Subtotal (5 items)
+        <span className="CartTotal-price">$2.234</span>
+      </h3>
+      <button>Proceed to checkout</button>
+    </div>
+  );
 };
 
 export default CartTotal;
